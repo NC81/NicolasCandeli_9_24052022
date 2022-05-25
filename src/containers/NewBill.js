188c94231dddcf5534c55c4004ae91167110cb69
NC <nicolas.candeli@cegetel.net>
2022-05-25 04:03:34 +0200

    Fix : [Bug Hunt] - Bills

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,26 +20,38 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+    const fileExtension = fileName.split(".").pop()
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    // Check if file extension is valid before creating new bill
+    switch (fileExtension) {
+      case 'jpg':
+      case 'jpeg':
+      case 'png':
+        formData.append('file', file)
+        formData.append('email', email)
+        this.store
+          .bills()
+          .create({
+            data: formData,
+            headers: {
+              noContentType: true
+            }
+          })
+          .then(({fileUrl, key}) => {
+            console.log(fileUrl)
+            this.billId = key
+            this.fileUrl = fileUrl
+            this.fileName = fileName
+          }).catch(error => console.error(error))
+        break;
+      default:
+        e.target.value = ''
+        console.error('Extension du fichier non valide')
+    }
   }
+
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
